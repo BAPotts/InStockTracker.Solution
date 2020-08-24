27c09ce9de10dd24ed096642247f0b0dbeec3cb5
Taylor Somers <tsomers@yandex.com>
2020-08-24 18:44:30 -0400

    Start building UI
    Co-authored-by: Micheal Hansen <micheal.hansen@gmail.com>
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>
    Co-authored-by: Joseph Pearce <joseph.pearce05@gmail.com>
    Co-authored-by: Beverly Potts <pottsbeverly@gmail.com>
    Co-authored-by: Taylor Somers <tsomers@yandex.com>

--- InStockTracker/Startup.cs
+++ InStockTracker/Startup.cs
@@ -49,7 +49,7 @@ namespace InStockTracker
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
