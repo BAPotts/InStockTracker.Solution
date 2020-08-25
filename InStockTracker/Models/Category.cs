1017a2ddd650caaae25e130ae538b0b4a1f0e210
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-08-25 09:08:32 -0700

    add data validation to category class
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- InStockTracker/Models/Category.cs
+++ InStockTracker/Models/Category.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace InStockTracker.Models
 {
@@ -9,6 +10,8 @@ namespace InStockTracker.Models
       this.Products = new HashSet<CategoryProduct>();
     }
     public int CategoryId { get; set; }
+    [Required(ErrorMessage = "Title is required")]
+    [StringLength(255)]
     public string Title { get; set; }
     public virtual ICollection<CategoryProduct> Products { get; set; }
   }
