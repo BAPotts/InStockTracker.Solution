ab1645dd1ebb343b40e1a869019c539558ede1eb
Deryck Jackson <44139628+DeryckJackson@users.noreply.github.com>
2020-08-25 09:18:22 -0700

    add more seed data to database
    Co-authored-by:  Joseph Pearce <joseph.pearce05@gmail.com>

--- InStockTracker/Models/InStockTrackerContext.cs
+++ InStockTracker/Models/InStockTrackerContext.cs
@@ -9,30 +9,36 @@ namespace InStockTracker.Models
     public DbSet<Category> Categories { get; set; }
     public DbSet<CategoryProduct> CategoryProduct { get; set; }
 
-    public InStockTrackerContext(DbContextOptions options) : base(options) {}
+    public InStockTrackerContext(DbContextOptions options) : base(options) { }
 
     protected override void OnModelCreating(ModelBuilder builder)
-      {
-        builder.Entity<Product>()
-        .HasData(
-          new Product { ProductId = 1, Name = "Webcam", Manufacturer = "Logitech", Description = "Logitech webcam", Stock = 10 },
-          new Product { ProductId = 2, Name = "Keyboard", Manufacturer = "Razer", Description = "Razer mechanical keyboard", Stock = 5 },
-          new Product { ProductId = 3, Name = "Mouse", Manufacturer = "Razer", Description = "Razer gaming mouse", Stock = 6 },
-          new Product { ProductId = 4, Name = "Microphone", Manufacturer = "Blue", Description = "Blue snowball microphone", Stock = 11 }
-        );
+    {
+      builder.Entity<Product>()
+      .HasData(
+        new Product { ProductId = 1, Name = "Webcam", Manufacturer = "Logitech", Description = "Logitech webcam", Price = 99.99m, Stock = 10 },
+        new Product { ProductId = 2, Name = "Keyboard", Manufacturer = "Razer", Description = "Razer mechanical keyboard", Price = 149.99m, Stock = 5 },
+        new Product { ProductId = 3, Name = "Mouse", Manufacturer = "Razer", Description = "Razer gaming mouse", Price = 49.99m, Stock = 6 },
+        new Product { ProductId = 4, Name = "Microphone", Manufacturer = "Blue", Description = "Blue snowball microphone", Price = 49.99m, Stock = 11 }
+      );
 
-        builder.Entity<Category>()
-        .HasData(
-          new Category { CategoryId = 1, Title = "Electronics" }
-        );
+      builder.Entity<Category>()
+      .HasData(
+        new Category { CategoryId = 1, Title = "Electronics" },
+        new Category { CategoryId = 2, Title = "Appliances" },
+        new Category { CategoryId = 3, Title = "Clothing" },
+        new Category { CategoryId = 4, Title = "Books" },
+        new Category { CategoryId = 5, Title = "Tools" },
+        new Category { CategoryId = 6, Title = "Home" },
+        new Category { CategoryId = 7, Title = "Garden" }
+      );
 
-        builder.Entity<CategoryProduct>()
-        .HasData(
-          new CategoryProduct { CategoryProductId = 1, ProductId = 1, CategoryId = 1 },
-          new CategoryProduct { CategoryProductId = 2, ProductId = 2, CategoryId = 1 },
-          new CategoryProduct { CategoryProductId = 3, ProductId = 3, CategoryId = 1 },
-          new CategoryProduct { CategoryProductId = 4, ProductId = 4, CategoryId = 1 }
-        );
-      }
+      builder.Entity<CategoryProduct>()
+      .HasData(
+        new CategoryProduct { CategoryProductId = 1, ProductId = 1, CategoryId = 1 },
+        new CategoryProduct { CategoryProductId = 2, ProductId = 2, CategoryId = 1 },
+        new CategoryProduct { CategoryProductId = 3, ProductId = 3, CategoryId = 1 },
+        new CategoryProduct { CategoryProductId = 4, ProductId = 4, CategoryId = 1 }
+      );
+    }
   }
 }
